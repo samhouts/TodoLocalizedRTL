5d35ac16e5632ded4fb3e937c01ef63469fdf8bd
Samantha Houts <samantha.houts@xamarin.com>
2018-04-06 10:44:01 -0700

    fork from https://github.com/xamarin/xamarin-forms-samples/tree/master/TodoLocalized

---
+++ TodoLocalized.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.IO;
+using Android.Speech.Tts;
+using Android.Content.PM;
+
+namespace TodoLocalized
+{
+	[Activity (Label = "@string/app_name", Icon = "@drawable/icon", MainLauncher = true, 
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : 
+	global::Xamarin.Forms.Platform.Android.FormsApplicationActivity 
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+			LoadApplication (new App ());
+		}
+	}
+}
---
+++ TodoLocalized/L10n.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms;
+
+namespace TodoLocalized
+{
+    public class L10n
+	{
+        const string ResourceId = "TodoLocalized.Resx.AppResources";
+
+        public static void SetLocale (CultureInfo ci)
+        {
+            DependencyService.Get<ILocale>().SetLocale(ci);
+		}
+
+		/// <remarks>
+		/// Maybe we can cache this info rather than querying every time
+		/// </remarks>
+		[Obsolete]
+		public static string Locale ()
+		{
+			return DependencyService.Get<ILocale>().GetCurrentCultureInfo().ToString();
+        }
+			
+		public static string Localize(string key, string comment)
+        {
+            //var netLanguage = Locale ();
+
+            // Platform-specific
+            ResourceManager temp = new ResourceManager(ResourceId, typeof(L10n).GetTypeInfo().Assembly);
+            Debug.WriteLine("Localize " + key);
+            string result = temp.GetString(key, DependencyService.Get<ILocale>().GetCurrentCultureInfo());
+
+            if (result == null)
+            {
+                result = key; // HACK: return the key, which GETS displayed to the user
+            }
+            return result;
+		}
+	}
+}
